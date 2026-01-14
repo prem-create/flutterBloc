e7b2194aeba21d0669279cfc55ceb2f8f6a42be9
prem-create <premdilliwar31@gmail.com>
2026-01-14 20:47:59 +0530

    inital commit

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,50 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryItems = [
+  {
+    "id": 1,
+    "name": "Rice",
+    "price": 60,
+    "quantity": "1 kg",
+    "category": "Grains",
+    "imageUrl":
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbbHMLbVwAUDT7B-oqUxYbrZguIHbIckk3yQ&s"
+  },
+  {
+    "id": 2,
+    "name": "Milk",
+    "price": 28,
+    "quantity": "500 ml",
+    "category": "Dairy",
+    "imageUrl":
+        "https://cdnasd.countrydelight.in/cdproductimg/new-website/Cow%20milk-450ml%20-PDP%20-3.jpg_1686642712633.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Eggs",
+    "price": 72,
+    "quantity": "12 pcs",
+    "category": "Poultry",
+    "imageUrl":
+        "https://images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-eggs-1296x728-feature.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Apple",
+    "price": 120,
+    "quantity": "1 kg",
+    "category": "Fruits",
+    "imageUrl":
+        "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce"
+  },
+  {
+    "id": 5,
+    "name": "Onion",
+    "price": 40,
+    "quantity": "1 kg",
+    "category": "Vegetables",
+    "imageUrl":
+        "https://images.unsplash.com/photo-1618512496248-a07fe83aa8cb"
+  }
+];
+
+}
