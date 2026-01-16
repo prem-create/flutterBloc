3fef6c2d5f5a70ff8013deb7765d292cd05872ab
prem-create <premdilliwar31@gmail.com>
2026-01-16 23:51:47 +0530

    format document

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_app/features/home/UI/home.dart';
 
-
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -11,9 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
